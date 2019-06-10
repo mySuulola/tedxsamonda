fc9f4fe0659fe14d1adcecfb481903e1affcb3cf
mySuulola <oluwaseyisuulola@gmail.com>
2019-06-10 02:24:20 +0100

    homepage

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var registerButton = document.getElementById('registerBtn')
+var registerForm = document.getElementById('registerForm')
+registerButton.addEventListener('click',  () => {
+  registerForm.className = 'd-block'
+  registerButton.style.display = 'none'
+
+} )
