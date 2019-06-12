fc014f49eca0b4cf4d486de7e019d7d5c6a9e195
mySuulola <oluwaseyisuulola@gmail.com>
2019-06-12 08:02:37 +0100

    responsiveness

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-var registerButton = document.getElementById('registerBtn')
-var registerForm = document.getElementById('registerForm')
-registerButton.addEventListener('click',  () => {
-  registerForm.className = 'd-block'
-  registerButton.style.display = 'none'
+// var registerButton = document.getElementById('registerBtn')
+// var registerForm = document.getElementById('registerForm')
+// registerButton.addEventListener('click',  () => {
+//   registerForm.className = 'd-block'
+//   registerButton.style.display = 'none'
 
-} )
+// } )
 
 
